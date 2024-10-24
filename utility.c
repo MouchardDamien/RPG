ca4c07a27686326dcdc52e7e86af6d53d0243aee
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2024-10-24 22:52:29 +0200

    Update utility.c

--- utility.c
+++ utility.c
@@ -9,3 +9,14 @@ void ClearScreen(){
 	printf("\033[H\033[J");
 }
 
+int getch(void) { 
+	struct termios oldattr, newattr; 
+	int ch; 
+	tcgetattr(STDIN_FILENO, &oldattr); 
+	newattr = oldattr; 
+	newattr.c_lflag &= ~(ICANON | ECHO); 
+	tcsetattr(STDIN_FILENO, TCSANOW, &newattr); 
+	ch = getchar(); 
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldattr); 
+	return ch; 
+}
