5b485e43e03efcb31a85f0c820132a2b9f997ec4
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2024-10-27 01:00:04 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,42 @@
 #include "header.h"
 
+int main(void) {
 
-int main(void){
+  ClearScreen();
+  
+  srand(time(NULL));
+  char choice_menu = -1;
 
-	srand(time(NULL));
-	
-	Player* p1 = malloc(sizeof(Player));
-	
-	if(p1 == NULL){
-		printf("Error of allocation in main !\n\n");
-	}
-	
-	ClearScreen();
-	init_player(p1);
+  Player *p1 = malloc(sizeof(Player));
 
-	return 0;
+  if (p1 == NULL) {
+    printf("Error of allocation in main !\n\n");
+  }
+
+  choice_menu = welcome();
+
+  char active = 1;
+
+  while(active){
+    switch(choice_menu){
+      case '1':
+        init_player(p1);
+        break;
+    
+      case '2':
+        rules();
+        choice_menu = main_menu();
+        break;
+    
+      case '3':
+        quit();
+        active = 0;
+        break;
+    
+      default:
+        break;
+    }
+  }
+  
+  return 0;
 }
-	
-	
-
